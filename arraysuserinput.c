ab8459414faee8af12aea490883245d17fdd308b
platy34 <eddiexu264@gmail.com>
2026-05-19 17:17:41 -0400

    arrayswithinput

---
+++ arraysuserinput.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int scores[5] = {0};
+    
+
+    for (int i = 0; i < 5; i++){
+        printf("Enter a score: ");
+        scanf("%d", &scores[i]);
+    }
+
+    for (int i = 0; i < 5; i++){
+        printf("%d ", scores[i]);
+    }
+
+    return 0;
+    
+
+}
